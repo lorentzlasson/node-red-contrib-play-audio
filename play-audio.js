0213cfcd08e2fe63fa0e5c5bee601572f9a3f600
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2015-09-19 18:23:08 +0200

    code dump

---
+++ play-audio.js
@@ -0,0 +1,13 @@
+var fs = require("fs");
+
+module.exports = function(RED) {
+	function  PlayAudioNode(config) {
+		RED.nodes.createNode(this,config);
+		var node = this;
+		this.on('input', function(msg) {
+			RED.comms.publish("playaudiows", msg.payload);
+		});
+	}
+	RED.nodes.registerType("play audio", PlayAudioNode);
+}
+
