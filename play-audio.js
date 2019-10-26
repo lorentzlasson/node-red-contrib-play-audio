d58d2ca235efa5240236fe5cef302a0b2a67d898
Dave Conway-Jones <conway@uk.ibm.com>
2019-10-26 13:35:30 +0100

    bump version for i18n content

--- play-audio.js
+++ play-audio.js
@@ -1,6 +1,6 @@
 /**
  * Copyright 2015 Lorentz Lasson
- * Copyright 2015, 2016 Lorentz Lasson, Dave Conway-Jones
+ * Copyright 2015-2019 Lorentz Lasson, Dave Conway-Jones
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
